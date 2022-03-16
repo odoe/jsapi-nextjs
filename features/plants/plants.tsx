50562630a1e3bd3a7c3a3da4e38d513bc889d74e
Rene Rubalcava <rrubalcava@esri.com>
2022-03-16 15:43:01 -0700

    add swr

--- features/plants/plants.tsx
+++ features/plants/plants.tsx
@@ -1,31 +1,40 @@
 import styles from '../../styles/Home.module.css'
+import useSWR from 'swr'
 import { useState, useEffect } from 'react'
 import { useAppSelector, useAppDispatch } from '../../app/hooks'
 import { updateTypes } from './plantsSlice'
 import Loader from '../../components/loader'
 import { useRouter } from 'next/router'
 
+const fetcher = async (
+    input: RequestInfo,
+    init: RequestInit,
+    ...args: any[]
+  ) => {
+        const res = await fetch(input, init)
+        return res.json()
+    }
+
 const Plants = () => {
+    const { data, error } = useSWR('/api/powerplants', fetcher)
     const types = useAppSelector((state) => state.plants.types)
     const dispatch = useAppDispatch()
-    const [isLoading, setLoading] = useState(false)
+    const [isLoading, setLoading] = useState(true)
     const router = useRouter()
 
     useEffect(() => {
         setLoading(true)
-        fetch('api/powerplants')
-        .then((res) => res.json())
-        .then((data) => {
+        if (data) {
             dispatch(updateTypes(data.types))
             setLoading(false)
-        })
-    }, [dispatch])
+        }
+    }, [data, error, dispatch])
 
     if (isLoading)
         return (
-        <div className={styles.loader}>
-            <Loader />
-        </div>
+            <div className={styles.loader}>
+                <Loader />
+            </div>
         )
     if (!types.length) return <p>No data</p>
 
