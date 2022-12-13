87fe453b857ce6ff2afd98fe26d002f4aceb8d7a
Rene Rubalcava <rrubalcava@esri.com>
2022-12-13 09:06:33 -0800

    feat: update for 4.25

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,21 @@
-import '../styles/globals.css'
-import type { ReactElement, ReactNode } from 'react'
-import type { NextPage } from 'next'
-import type { AppProps } from 'next/app'
-import { Provider } from 'react-redux'
-import store from '../app/store'
+import "../styles/globals.css";
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import store from "../app/store";
 
 type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
+  Component: any;
+};
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page)
+  const getLayout = Component.getLayout ?? ((page: any) => page);
   return (
-    <Provider store={store}>
-      {getLayout(<Component {...pageProps} />)}
-    </Provider>
-  )
+    <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
+  );
 }
